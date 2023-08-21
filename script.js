c1b220b42b66c535e022923d8af21deea6a16e32
Louis Lemaire <louis.lemaire@gmail.com>
2023-08-21 16:33:51 -0400

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css
    Added changes to sizepicker functionality

--- script.js
+++ script.js
@@ -17,12 +17,22 @@
 const gridContainer = document.getElementById('gridContainer');
 
 // Define the number of rows and columns in the grid
-const numRows = 64;
-const numCols = 64;
+const numRows = 16;
+const numCols = 16;
 let etchMode = 'classic'; //mode can be classic, rainbow, or gradient
 
 // Create grid items and add them to the container
 function generateEtchASketch(rows, cols, mode) {
+    //clear the old grid (because we will regenerate later
+    let element = document.getElementById("gridContainer");
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }    
+    //set the grid parameters with CSS (template rows and columns)
+    gridContainer.style.gridTemplateColumns = `repeat(${cols}, calc(500px/${cols}))`;
+    gridContainer.style.gridTemplateRows = `repeat(${rows}, calc(500px/${rows}))`;
+
+    //now generate the grid
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < cols; col++) {
             const gridItem = document.createElement('div');
@@ -37,3 +47,12 @@ function generateEtchASketch(rows, cols, mode) {
     }
 }
 generateEtchASketch(numRows, numCols, etchMode);
+
+const sizeButton = document.getElementById('sizePicker');
+
+sizeButton.addEventListener('click', () => {
+    //need to check that the input is a number
+    // and also that it stays below 128, otherwise the browser has trouble
+    const size = prompt("Enter the grid size");
+    generateEtchASketch(size, size, etchMode);
+});
